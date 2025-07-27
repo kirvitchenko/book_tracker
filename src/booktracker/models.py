1f36baf2d0f14d3a610f46674eca25388e3e11b2
kirvitchenko <vitchenkokir@gmail.com>
2025-07-27 15:36:16 +0300

    create models and initial migrations

---
+++ src/booktracker/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author_id = models.ManyToManyField('Author')
+    genre_id = models.ManyToManyField('Genre')
+    start_reading_date = models.DateField(default=timezone.now)
+    end_reading_date = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Note(models.Model):
+    text = models.TextField()
+    book = models.ForeignKey('Book', on_delete=models.CASCADE)
+    writing_datetime = models.DateTimeField(auto_now_add=True)
+    last_edit_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        if len(self.text) > 50:
+            return f'{self.text[:50]}...'
+        return self.text
