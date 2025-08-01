8348d85af25d44f5dca9a2b90509b36af0e128af
kirvitchenko <vitchenkokir@gmail.com>
2025-08-01 17:03:51 +0300

    refactor book and user and add docstring

--- src/booktracker/models.py
+++ src/booktracker/models.py
@@ -8,24 +8,42 @@ from django.utils import timezone
 # Create your models here.
 
 class User(AbstractUser):
+    """
+    Модель пользователя которая наследуюется от Abstractuser и описывает дополниться поля:
+    - avatar - аватар
+    - bitrh_date - дату рождения
+    - book - связь с моделью Book через модель UserBook
+    """
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
     birth_date = models.DateField(blank=True, null=True)
+    book = models.ManyToManyField('Book', through='UserBook')
 
     def __str__(self):
         return self.username
 
 
 class Book(models.Model):
+    """
+    Модель которая описывает книгу в системе и содержит следующие поля:
+    title - название
+    author - автор
+    genre = жанр
+    """
     title = models.CharField(max_length=100)
     author = models.ManyToManyField('Author')
     genre = models.ManyToManyField('Genre')
-    user = models.ManyToManyField(User, through='UserBook')
 
     def __str__(self):
         return self.title
 
 
 class UserBook(models.Model):
+    """Модель которая используется для связи книги и пользователся:
+    user - пользователь по внешнему ключу
+    book - по внешнему ключу
+    start_ и end_reading_date - дата начала и окончания чтения
+    rating - личная оценка пользователем книги от 1 до 10
+    """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     start_reading_date = models.DateField(default=timezone.now)
@@ -33,6 +51,9 @@ class UserBook(models.Model):
     rating = models.PositiveIntegerField(validators=[MaxValueValidator(10)])
 
     class Meta:
+        """
+        Класс который предотвращает дублирование записей
+        """
         constraints = [
             models.UniqueConstraint(
                 fields=["user", "book"], name="unique_user_book"
@@ -41,6 +62,9 @@ class UserBook(models.Model):
 
 
 class Author(models.Model):
+    """
+    Модель автора котоаря содержит только его имя
+    """
     name = models.CharField(max_length=100)
 
     def __str__(self):
@@ -48,6 +72,9 @@ class Author(models.Model):
 
 
 class Genre(models.Model):
+    """
+    Модель жанра котоаря содержит только его название
+    """
     name = models.CharField(max_length=100)
 
     def __str__(self):
@@ -55,6 +82,13 @@ class Genre(models.Model):
 
 
 class Note(models.Model):
+    """
+    Модель заметки которая содержит следующие поля
+    text - сам текст заметки
+    book - внешний ключ по которому замтека привязывается к книге
+    writing_datetime - дата и время написания заметки
+    last_edit_time - последняя редакция заметки
+    """
     text = models.TextField()
     book = models.ForeignKey('Book', on_delete=models.CASCADE)
     writing_datetime = models.DateTimeField(auto_now_add=True)
--- src/config/settings.py
+++ src/config/settings.py
@@ -131,7 +131,8 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-
+"""переменная для аутентификации пользователя"""
 AUTH_USER_MODEL = 'booktracker.User'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
